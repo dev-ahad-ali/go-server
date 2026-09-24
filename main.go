0fb35c65d0ff08078d0facb91c023e487e748d32
dev-ahad-ali <ahadali.dev@gmail.com>
2026-09-24 21:01:42 +0600

    another handle func

--- main.go
+++ main.go
@@ -9,9 +9,14 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, World!")
 }
 
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Text about information....")
+}
+
 func main() {
 
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/hello", helloHandler)
+	mux.HandleFunc("/about", aboutHandler)
 }
